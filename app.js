9d0a28cdd2cd7cb3d20a884b16558fa3979e8239
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2022-04-29 04:34:36 +0100

    Set up routes.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect")
+const productRouter = require("./routes/products")
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -13,6 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("<h1>Store API </h1><a href='/api/v1/products'>Product Route</a>")
 });
+
+app.use("/api/v1/products",  productRouter);
 
 
 app.use(notFoundMiddleware);
