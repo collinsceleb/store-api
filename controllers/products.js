a31622ca8129f3e49f0824f1ce92bd5c932cdbc2
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2022-05-02 07:53:54 +0100

    Implemented query functionalities for products.

--- controllers/products.js
+++ controllers/products.js
@@ -8,8 +8,21 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
+    const { featured, company, name } = req.query
+    const queryObject = {};
+    if (featured) {
+        queryObject.featured = featured ==='true'? true : false
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'e'};
+    }
+    const products = await Product.find(queryObject);
     res.status(200).json({
-        msg: "Products route"
+        products,
+        nbHits: products.length
     })
 }
 
