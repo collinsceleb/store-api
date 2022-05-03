eef19d31a6a455467f0b058f7ae32f9505093cc7
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2022-05-03 07:37:02 +0100

    Implemented sort and select functionalities.

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product")
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({featured: true})
+    const products = await Product.find({}).select('name price')
     res.status(200).json({
         products,
         nbHits: products.length
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured ==='true'? true : false
@@ -17,9 +17,26 @@ const getAllProducts = async (req, res) => {
         queryObject.company = company;
     }
     if (name) {
-        queryObject.name = { $regex: name, $options: 'e'};
+        queryObject.name = { $regex: name, $options: 'i'};
     }
-    const products = await Product.find(queryObject);
+    let result = Product.find(queryObject);
+
+    // Sorting
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);        
+    }else {
+        result = result.sort('createdAt')
+    }
+
+    // Selecting
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList);
+    } else {
+        
+    }
+    const products = await result;
     res.status(200).json({
         products,
         nbHits: products.length
