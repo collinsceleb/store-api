588546f662872f091d776773ec154dd7570b44e6
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2022-05-04 06:51:13 +0100

    Implemented limit and skip functionalities.

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,10 @@
 const Product = require("../models/product")
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price')
+        .limit(10)
+        .skip(5)
     res.status(200).json({
         products,
         nbHits: products.length
@@ -33,9 +37,12 @@ const getAllProducts = async (req, res) => {
     if (fields) {
         const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList);
-    } else {
-        
     }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit);
+    
     const products = await result;
     res.status(200).json({
         products,
